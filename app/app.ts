cf40c84fc802cbf9b24bb91b09398388b4ac7266
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-11-28 09:29:28 +0100

    Add initial tsconfig.json.

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 function startGame() {
   // starting a new game
   var messagesElement = document.getElementById("messages");
-  messagesElement.innerText = "Welcome Ties!!!";
+  messagesElement.innerText = "Welcome Ties!!! Starting new game...";
 }
 
 document.getElementById("startGame").addEventListener("click", startGame);
